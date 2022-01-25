203a876fc34011e05472bd2bc16cbe64e9535a4b
MuffinPlease <benjamintec3455@gmail.com>
2022-01-25 13:32:43 -0600

    Class 4 routing

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) =>{
   res.send('Hello express server!');
 });
 
+app.get('/new-route', (req, res) => {
+  res.send(`Hello, I'm the new route`);
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000,
+  })
+})
+
 app.listen(port, () => {
   console.log('Server started on ' + port);
 });
